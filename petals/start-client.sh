bf4a5b52de67f97e51988c0fd80a1c8b610e8449
Mamoutou <mamoutou.diarra@hivenet.com>
2024-01-22 22:50:51 +0000

    use petals-cache volume

--- petals/start-client.sh
+++ petals/start-client.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo docker container stop public-client
 sudo docker container remove public-client
-sudo docker create -p 8080:8080 --ipc host --gpus 1 --volume petals-cache3:/root/.cache --name public-client hive-chat:mistral
+sudo docker create -p 8080:8080 --ipc host --gpus 1 --volume petals-cache:/root/.cache --name public-client hive-chat:mistral
 sudo docker container start public-client
